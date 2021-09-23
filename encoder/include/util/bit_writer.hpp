577167bf0c663e614ed9f9d36e483d5bf862f133
Kuba Marek <blue.cube@seznam.cz>
2021-09-23 22:42:30 +0200

    Utility for writing single bit values

---
+++ encoder/include/util/bit_writer.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+#include <limits>
+#include <utility>
+#include <cassert>
+
+namespace tablog::util {
+
+template <typename OutputF>
+class BitWriter {
+private:
+    using BufferT = uint_fast8_t;
+public:
+    BitWriter(OutputF output)
+        : output(std::move(output)) {}
+
+    /// Write bitCount least significant bits of data into the output.
+    template <typename T>
+    void write(T data, uint_fast8_t bitCount = std::numeric_limits<T>::digits) noexcept {
+        static_assert(std::is_integral_v<T>, "Only integral types can be written");
+        static_assert(!std::is_signed_v<T>, "Only unsigned types can be written");
+        assert(bitCount <= std::numeric_limits<T>::digits);
+
+
+        if (bitCount + bufferUsed < outputBitSize) {
+            // Not enough data to output a byte
+            buffer |= data << bufferUsed;
+            bufferUsed += bitCount;
+        } else {
+            // First use beginning of the data to fill the rest of the buffer
+            buffer |= (static_cast<BufferT>(data) << bufferUsed) & outputMask;
+            output(buffer);
+            auto shift = outputBitSize - bufferUsed;
+            data >>= shift;
+            bitCount -= shift;
+
+            // Then output all the full bytes in the data input
+            while (bitCount >= outputBitSize) {
+                output(data & outputMask);
+                data >>= outputBitSize;
+                bitCount -= outputBitSize;
+            }
+
+            // Set the buffer to the remainder.
+            buffer = data & ((1 << bitCount) - 1);
+            bufferUsed = bitCount;
+        }
+
+        assert(bufferUsed < outputBitSize);
+        buffer &= ((1 << bufferUsed) - 1);
+        assert(buffer >> bufferUsed == 0);
+    }
+
+    /// Pad the last unwritten byte with zeros and output it.
+    void flush() {
+        output(buffer);
+        buffer = 0;
+        bufferUsed = 0;
+    }
+
+private:
+    static constexpr uint_fast8_t outputBitSize = 8;
+    static constexpr uint_fast8_t outputMask = 0xff;
+
+    BufferT buffer = 0;
+    uint_fast8_t bufferUsed = 0;
+
+    OutputF output;
+};
+
+}
---
+++ encoder/tests/unit/bit_writer.cpp
@@ -0,0 +1,61 @@
+#include "util/bit_writer.hpp"
+
+#include <catch2/catch.hpp>
+
+struct StringWriter {
+    void operator() (unsigned c) {
+        s.push_back(c);
+    }
+
+    std::string& s;
+};
+
+TEST_CASE("BitWriter") {
+    std::string data;
+
+    tablog::util::BitWriter bw(StringWriter{data});
+
+    SECTION("Write 8 single bits") {
+        for (uint32_t i = 0; i < 8; ++i)
+            bw.write(i, 1);
+        REQUIRE(data == "\xaa");
+    }
+
+    SECTION("Write 8 bits") {
+        bw.write(static_cast<uint8_t>('A'), 8);
+        REQUIRE(data == "A");
+    }
+
+    SECTION("Write 4x4 bits") {
+        bw.write(0x6u, 4);
+        bw.write(0x7u, 4);
+        bw.write(0x8u, 4);
+        bw.write(0x9u, 4);
+        REQUIRE(data == "\x76\x98");
+    }
+
+    SECTION("Write 32bit number -- checks endiannes") {
+        bw.write(327348935ull, 32);
+        REQUIRE(data == "\xc7\xf2\x82\x13");
+    }
+
+    SECTION("Write 2x16bit number -- checks endiannes") {
+        /// The same bytes as for 32bit write above, just split up
+        bw.write(62151u, 16);
+        bw.write(4994u, 16);
+        REQUIRE(data == "\xc7\xf2\x82\x13");
+    }
+
+    SECTION("Flush 4 bits") {
+        bw.write(0x9u, 4);
+        bw.flush();
+        REQUIRE(data == "\x09");
+    }
+
+    SECTION("Small write followed by large write followed by flush") {
+        bw.write(0u, 1);
+        bw.write(2474832583ull, 32);
+        bw.flush();
+        REQUIRE(data == "\x8e\xe5\x05\x27\x01");
+    }
+}
