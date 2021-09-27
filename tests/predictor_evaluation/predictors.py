dc31f8d00bfab90ce013ddf05676d33388d33d7e
Kuba Marek <blue.cube@seznam.cz>
2021-09-27 11:08:17 +0200

    HTML table for predictor overviews

--- tests/predictor_evaluation/predictors.py
+++ tests/predictor_evaluation/predictors.py
@@ -2,6 +2,7 @@
 
 import math
 import operator
+import itertools
 
 from datasets import individual_datasets
 from decoder import predictors
@@ -19,48 +20,76 @@ def evaluate_predictor_dataset(predictor, dataset):
 
     return score, count
 
+def open_datasets():
+    yield from individual_datasets(include_synthetic=False)
+
 def evaluate_predictors(*predictor_factories):
-    predictor_results = {}
-    dataset_results = {}
+    results = {}
+    predictors = []
+    datasets = [dataset.name for dataset in open_datasets()]
 
     for predictor_factory in predictor_factories:
         predictor = predictor_factory.__name__
-        print(f"{predictor}:")
-        score_sum = 0
-        total_count = 0
-        for dataset in individual_datasets(include_synthetic=False):
+        predictors.append(predictor)
+
+        for dataset in open_datasets():
             assert len(dataset.field_types) == 1
             score, count = evaluate_predictor_dataset(
                 predictor_factory(dataset.field_types[0]),
                 dataset
             )
-            avg_score = score / count
-            percent_score = 100 * avg_score / int(dataset.field_types[0][1:])
-            print(f"  {dataset.name}: {avg_score:.1f} ({percent_score:.0f}%)")
-            score_sum += score
+            results[dataset.name, predictor] = (score, count)
+
+    table_header = ["Dataset"]
+    table_header.extend(predictors)
+    table_header.append("Average")
+
+    table_data = []
+    for dataset_name in datasets:
+        row = [dataset_name]
+        total_score = 0
+        total_count = 0
+        for predictor_name in predictors:
+            score, count = results[dataset_name, predictor_name]
+            total_score += score
             total_count += count
+            row.append(f"{score / count:.1f}")
+        row.append(f"{total_score / total_count:.1f}")
+        table_data.append(row)
 
-            try:
-                dataset_score_sum, dataset_total_count = dataset_results[dataset.name]
-            except KeyError:
-                dataset_score_sum = 0
-                dataset_total_count = 0
-            dataset_score_sum += score
-            dataset_total_count += count
-            dataset_results[dataset.name] = (dataset_score_sum, dataset_total_count)
+    row = ["Average"]
+    for predictor_name in predictors:
+        total_score = 0
+        total_count = 0
+        for dataset_name in datasets:
+            score, count = results[dataset_name, predictor_name]
+            total_score += score
+            total_count += count
+        row.append(f"{total_score / total_count:.1f}")
+    row.append("")
+    table_data.append(row)
 
-        predictor_results[str(predictor)] = score_sum / total_count
+    column_widths = [len(x) for x in table_header]
+    for row in table_data:
+        assert(len(column_widths) == len(row))
+        column_widths = [max(w, len(s)) for w, s in zip(column_widths, row)]
 
-    print()
-    print("Total:")
+    def tag(s, w, tag_name):
+        return f"<{tag_name}>{s}</{tag_name}>".rjust(w + 5 + 2 * len(tag_name))
 
-    for dataset_name, (score_sum, total_count) in dataset_results.items():
-        avg_score = score_sum / total_count
-        print(f"  {dataset_name}: {avg_score:.1f}")
-
-    print()
-    for predictor_name, avg_score in sorted(predictor_results.items(), key=operator.itemgetter(1)):
-        print(f"  {predictor_name}: {avg_score:.1f}")
+    print("<table><thead>")
+    print("<tr>", end='')
+    for h, w in zip(table_header, column_widths):
+        print(tag(h, w, "th"), end='')
+    print("</tr>")
+    print("</thead><tbody>")
+    for row in table_data:
+        print("<tr>", end='')
+        print(tag(row[0], column_widths[0], "th"), end='')
+        for s, w in zip(row[1:], column_widths[1:]):
+            print(tag(s, w, "td"), end='')
+        print("</tr>")
+    print("</tbody></table>")
 
 
 evaluate_predictors(
