19b0b7454952ae90f94a2acc0d5d65a52aa3b4ad
Kuba Marek <blue.cube@seznam.cz>
2021-10-25 21:53:53 +0200

    First version of an adaptive linear predictor.

--- encoder/include/predictors.hpp
+++ encoder/include/predictors.hpp
@@ -27,4 +27,42 @@ protected:
     util::CircularBufferFixed<T, N, uint_fast8_t> history;
 };
 
+/// Keeps a saturating counter that switches between two modes:
+/// 1. Predict no change of value
+/// 2. Predict a constant first derivative based on last two values
+/// Predicts next value by looking at the last two values
+template <typename T>
+class Linear12Adapt {
+public:
+    using Type = T;
+
+    /// Returns a predicted value
+    /// Prediction always works, regardless of number of data points provided.
+    T predict_and_feed(T value) {
+        const auto prediction1 = prev[1];
+        const auto prediction2 = 2 * prev[1] - prev[0]; // Might overflow, but that's ok.
+
+        const auto prediction = selector >= 0 ? prediction1 : prediction2;
+
+        const auto error1 = abs_diff(prediction1, value);
+        const auto error2 = abs_diff(prediction2, value);
+
+        if (error1.first < error2.first) {
+            if (selector < (selectorLimit - 1))
+                ++selector;
+        } else if (error1.first > error2.first) {
+            if (selector > -selectorLimit)
+                --selector;
+        }
+
+        prev[0] = prev[1];
+        prev[1] = value;
+    }
+
+protected:
+    static constexpr int_fast8_t selectorLimit = 8;
+    T prev[2];
+    int_fast8_t selector;
+};
+
 }
