3d35e8f81f3e182fb5c03339bbddcc2ebd2ffcab
Kuba Marek <blue.cube@seznam.cz>
2021-11-21 18:28:50 +0100

    Refactored parts of stream encoder functionality to easily testable functions

--- encoder/include/stream_encoder.hpp
+++ encoder/include/stream_encoder.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include "util/misc.hpp"
 #include "util/bit_writer.hpp"
+#include "stream_encoder_bits.hpp"
 
 #include <cstdint>
 #include <limits>
@@ -22,14 +22,15 @@ public:
         : output(std::move(output)) {}
 
     void header(uint_fast8_t version, uint_fast8_t fieldCount) {
-        elias_gamma(version + 1u);
-        elias_gamma(fieldCount);
+        assert(fieldCount > 0);
+        elias_gamma(version, output);
+        elias_gamma(fieldCount - 1u, output);
     }
 
     /// Output field header. Field headers must directly follow header, with the
     /// same count as fieldCount in the header.
     void field_header(const char* fieldName, bool signedType, uint_fast8_t typeSize) {
-        string(fieldName);
+        encode_string(fieldName, output);
         uint_fast8_t typeExponent = small_int_log2(typeSize);
         output.write_bit(signedType);
         output.write(typeExponent, 2);
@@ -51,7 +52,7 @@ public:
         output.write_bit(0);
         output.write_bit(predictionHigh);
 
-        adaptive_exp_golomb(absErrorToEncode - 1, streamState);
+        adaptive_exp_golomb(absErrorToEncode - 1, streamState, output);
     }
 
     void end_of_stream() {
@@ -59,55 +60,6 @@ public:
     }
 
 private:
-    static constexpr uint_fast8_t streamStateShift = 2;
-
-    /// Encode the number using adaptive exp-golomb coding.
-    /// This function can encode zero values.
-    ///
-    /// Loosely based on
-    /// Henrique S. Malvar: Adaptive Run-Length / Golomb-Rice Encoding of
-    ///     Quantized Generalized Gaussian Sources with Unknown Statistics
-    /// https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/Malvar_DCC06.pdf
-    ///
-    /// We use the EXP variant, because for anomalous inputs the non-exp version
-    /// could fail catastrophically.
-    /// Eg. 64 bit value, that has low values and low prediction errors, suddenly
-    /// jumping to INT64_MAX. This would cause the unary part of golomb coding to
-    /// explode to 2**64 bits of length.
-    template <typename T>
-    void adaptive_exp_golomb(T n, uint8_t& streamState) {
-        const auto k = streamState >> streamStateShift;
-        const auto p = n >> k;
-
-        elias_gamma(p + 1); // Quotient; elias gamma can't encode zeros, therefore + 1
-        output.write(n, k); // Remainder (masking upper bits is performed by bit writer)
-
-        streamState += p - 1; // Update the adaptive shift value
-    }
-
-    template <typename T>
-    void elias_gamma(T n) {
-        static_assert(!std::is_signed_v<T>);
-        assert(n > 0);
-
-        auto logN = int_log2(n); // TODO: use std::bit_width(n) - 1, once we bump to c++20
-        output.write(T(0), logN);
-        output.write(n, logN - 1);
-    }
-
-    /// Encode the string into the output as a sequence
-    /// Null string is equivalent to empty string.
-    void string(const char* str) {
-        (void)str;
-        /*if (str) {
-            ValueCompressor<uint8_t, predictors::Constant<uint8_t, 'e'>> compressor;
-            number(strlen(str));
-            while (*fieldName)
-                compressor.write(*(fieldName++), *this);
-        }
-        else
-            number((uint8_fast_t)0);*/
-    }
 
     util::BitWriter<OutputF> output;
 };
---
+++ encoder/include/stream_encoder_bits.hpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <cassert>
+#include <cstdint>
+#include <type_traits>
+
+#include "util/misc.hpp"
+
+namespace tablog::detail {
+
+/// Encode a positive number using Elias gamma encoding,
+/// offset by 1, so that we can store zero
+template <typename T, typename BW>
+inline void elias_gamma(T n, BW& bitWriter) {
+    static_assert(!std::is_signed_v<T>);
+
+    ++n;
+
+    auto logN = int_log2(n); // TODO: use std::bit_width(n) - 1, once we bump to c++20
+    bitWriter.write(T(0), logN);
+    bitWriter.write(n, logN - 1);
+}
+
+/// Encode the number using adaptive exp-golomb coding.
+/// This function can encode zero values.
+///
+/// Loosely based on
+/// Henrique S. Malvar: Adaptive Run-Length / Golomb-Rice Encoding of
+///     Quantized Generalized Gaussian Sources with Unknown Statistics
+/// https://www.microsoft.com/en-us/research/wp-content/uploads/2016/02/Malvar_DCC06.pdf
+///
+/// We use the EXP variant, because for anomalous inputs the non-exp version
+/// could fail catastrophically.
+/// Eg. 64 bit value, that has low values and low prediction errors, suddenly
+/// jumping to INT64_MAX. This would cause the unary part of golomb coding to
+/// explode to 2**64 bits of length.
+///
+/// \arg streamState An auxiliary state variable that describes the typical
+///      range of the encoded values and adapts to the input values.
+template <typename T, typename BW>
+void adaptive_exp_golomb(T n, uint8_t& streamState, BW& bitWriter) {
+    static_assert(!std::is_signed_v<T>);
+
+    constexpr uint_fast8_t streamStateShift = 2;
+
+    const auto k = streamState >> streamStateShift;
+    const auto p = n >> k;
+
+    elias_gamma(p, bitWriter); // Quotient; elias gamma can't encode zeros, therefore + 1
+    bitWriter.write(n, k); // Remainder (masking upper bits is performed by bit writer)
+
+    streamState += p - 1; // Update the adaptive shift value
+}
+
+/// Encode the string into the output as a sequence
+/// Null string is equivalent to empty string.
+template <typename BW>
+inline void encode_string(const char* str, BW& bitWriter) {
+    (void)str;
+    (void)bitWriter;
+
+    /*if (str) {
+        ValueCompressor<uint8_t, predictors::Constant<uint8_t, 'e'>> compressor;
+        number(strlen(str));
+        while (*fieldName)
+            compressor.write(*(fieldName++), *this);
+    }
+    else
+        number((uint8_fast_t)0);*/
+}
+
+}
