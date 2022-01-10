6b7cccd5f482f1e65124626705e219b59ae70c75
Kuba Marek <blue.cube@seznam.cz>
2022-01-10 18:14:51 +0100

    Added high level framing decoding logic

--- decoder/framing.py
+++ decoder/framing.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from collections.abc import Iterable
 from typing import Union, Literal
 import itertools
@@ -50,3 +52,89 @@ def decode_framing_raw(data: Union[bytes, Iterable[bytes]]) -> Union[Literal[blo
         # Output the last escape byte if there was one
         yield escape_byte
 
+
+def decode_framing(data: Union[bytes, Iterable[bytes]]) -> Iterable[Union[FramingError, FramingBlockIterator]]:
+    """ Iterates over framing block iterators (see FramingBlockIterator),
+    delimited by start and end markers (delimiters are not included in block data).
+    Yields FramingError instances when encountering framing errors. """
+
+    raw_framing = decode_framing_raw(data)
+
+    while True:
+        junk_size, found = _consume_until_marker(raw_framing, block_start_marker)
+        if junk_size != 0:
+            yield UnexpectedCharacters(junk_size)
+        if not found:
+            break
+
+        block = FramingBlockIterator(raw_framing)
+        yield block
+
+        frame_error = block._ensure_consumed()
+
+        if frame_error is not None:
+            yield frame_error
+
+
+def _consume_until_marker(it, marker):
+    """ Consume data from the iterator, until marker is found (compared using
+    operator `is`).
+    Returns tuple of number of elements consumed before the marker and bool
+    indicating whether the marker was found (False means the iterator was
+    exhausted. """
+    i = -1
+    for i, v in enumerate(it):
+        if v is marker:
+            return (i, True)
+    return (i + 1, False)
+
+
+class FramingBlockIterator:
+    _empty_iterator = iter(())
+
+    def __init__(self, it):
+        self._it = it
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        v = next(self._it)
+        if v is block_end_marker:
+            self._it = self._empty_iterator
+            raise StopIteration()
+        return v
+
+    def _ensure_consumed(self):
+        """ Consume all data up to the following block end marker unless the block
+        has already been consumed.
+        Returns an error object if there was a problem, None otherwise. """
+
+        if self._it is self._empty_iterator:
+            return None
+
+        _, found = _consume_until_marker(self._it, block_end_marker)
+        self._it = self._empty_iterator
+
+        if found:
+            return None
+        else:
+            return UnexpectedEndOfData()
+
+
+class FramingError:
+    def __str__(self):
+        return "Unspecified framing error"
+
+
+class UnexpectedCharacters(FramingError):
+    def __init__(self, n):
+        self.n = n
+
+    def __str__(self):
+        return f"Found {self.n} unexpected characters before block start"
+
+
+class UnexpectedEndOfData(FramingError):
+    def __str__(self):
+        return "Input data ended unexpectedly"
--- decoder/tests/framing_test.py
+++ decoder/tests/framing_test.py
@@ -1,6 +1,8 @@
 import hypothesis
 import pytest
 
+import collections.abc
+
 from decoder import framing
 
 
@@ -45,3 +47,95 @@ def test_raw_two_escape():
 
 def test_raw_two_commands():
     assert list(framing.decode_framing_raw(b"Tll")) == [framing.block_start_marker, ord("l")]
+
+
+def flattened_framing(b):
+    """ Parses b using framing decoder and converts its output to a flattened byte
+    string, that captures the complete structure of the decoded framing.
+    To keep the flattened format simple, there is no escaping and only numbers are
+    supposed to be used as the actual data inside the framed blocks """
+    output = []
+    for item in framing.decode_framing(b):
+        if isinstance(item, framing.UnexpectedCharacters):
+            output.append(b"C")
+        elif isinstance(item, framing.UnexpectedEndOfData):
+            output.append(b"X")
+        elif isinstance(item, framing.FramingError):
+            output.append(b"E")
+        else:
+            assert isinstance(item, collections.abc.Iterator)
+            output.append(b"(")
+            output.append(bytes(item))
+            output.append(b")")
+
+    return b"".join(output)
+
+
+def test_one_block():
+    assert flattened_framing(b"Tl123T#") == b"(123)"
+
+
+@hypothesis.strategies.composite
+def flattened_block_decoding_inputs(draw):
+    """ This is a test strategy that generates a bytes input and an expected flattened
+    output pairs """
+    data_strategy = hypothesis.strategies.text(alphabet="123", max_size=3) \
+        .map(lambda x: x.encode("ascii"))
+    junk_strategy = hypothesis.strategies.text(alphabet="123#", max_size=5) \
+        .map(lambda x: x.replace("!", "T#").encode("ascii"))
+    block_count = draw(hypothesis.strategies.integers(min_value=0, max_value=3))
+    block_data = draw(hypothesis.strategies.lists(
+        data_strategy, min_size=block_count, max_size=block_count
+    ))
+
+    junk_before = draw(junk_strategy)
+    if block_count:
+        gap_junk = draw(hypothesis.strategies.lists(
+            junk_strategy, min_size=block_count - 1, max_size=block_count - 1
+        ))
+        incomplete_last_block = draw(hypothesis.strategies.booleans())
+        if incomplete_last_block:
+            junk_after = b""
+        else:
+            junk_after = draw(junk_strategy)
+
+    input_parts = []
+    expected_parts = []
+
+    if len(junk_before):
+        input_parts.append(junk_before)
+        expected_parts.append(b"C")
+    if block_count:
+        for block, junk in zip(block_data[:-1], gap_junk):
+            input_parts.append(b"Tl")
+            input_parts.append(block)
+            input_parts.append(b"T#")
+            expected_parts.append(b"(")
+            expected_parts.append(block)
+            expected_parts.append(b")")
+
+            if len(junk):
+                input_parts.append(junk)
+                expected_parts.append(b"C")
+
+        input_parts.append(b"Tl")
+        input_parts.append(block_data[-1])
+        expected_parts.append(b"(")
+        expected_parts.append(block_data[-1])
+        expected_parts.append(b")")
+
+        if incomplete_last_block:
+            expected_parts.append(b"X")
+        else:
+            input_parts.append(b"T#")
+            if len(junk_after):
+                input_parts.append(junk_after)
+                expected_parts.append(b"C")
+
+    return (b"".join(input_parts), b"".join(expected_parts))
+
+
+@hypothesis.given(flattened_block_decoding_inputs())
+def test_block_decoding(data):
+    input_data, expected_flattened = data
+    assert flattened_framing(input_data) == expected_flattened
