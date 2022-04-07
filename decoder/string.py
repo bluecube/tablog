5ebce8c23567c4e83ac29fb5b42fbf8aa060b256
Kuba Marek <blue.cube@seznam.cz>
2022-04-07 22:41:05 +0200

    Started working on string decoder

---
+++ decoder/string.py
@@ -0,0 +1,18 @@
+
+# The following code was generated from the jupyter notebook string_predictors.ipynb
+# fmt: off
+decoder_trie = (
+    ((('i', (('g', ('ed', ('tion', 'tio'))), 'd')), ('o', (('in',
+    ('te', ((('ht', ((((('htt', '.c'), ':'), (':/', ';')), '.co'),
+    ',')), 'http'), '.'))), ((('://', ';\n'), ', '), ('-', '. '))))),
+    (('a', (('p', ('or', 'v')), 'l')), ((((('ce', 'ne'), 'ti'), ('es',
+    'nd')), ((('ea', 'ra'), ('co', 'ic')), (('ro', 'ion'), 'en'))),
+    't'))), ((((('f', ((('atio', 'ati'), 'ri'), ('de', 'hi'))), ('he',
+    ('at', ('ing', 'me')))), (('m', ' '), '_')), (('h', ('th', ('1',
+    ('ve', 'le')))), (((('io', 'as'), 'b'), 'u'), (('on', ('ou',
+    'se')), ('re', ('ha', 'and')))))), (('e', (((('ng', ('.com',
+    'ent')), 'an'), ('w', ('st', 'to'))), 'r')), (('s', ('c', ('er',
+    'y'))), (((('nt', 'ar'), ('al', 'it')), ('the', ('is', 'of'))),
+    'n'))))
+)  # noqa: E128
+# fmt: on
